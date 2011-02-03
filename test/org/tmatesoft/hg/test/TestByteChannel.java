4575e2d4473b1a54851951b22c80020be9e476b4
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2011-02-03 23:37:21 +0100

    Test content of each and every revision

--- test/org/tmatesoft/hg/test/TestByteChannel.java
+++ test/org/tmatesoft/hg/test/TestByteChannel.java
@@ -19,11 +19,9 @@ package org.tmatesoft.hg.test;
 import java.util.Arrays;
 
 import org.junit.Assert;
-import org.tmatesoft.hg.core.CatCommand;
 import org.tmatesoft.hg.core.RepositoryFacade;
 import org.tmatesoft.hg.internal.ByteArrayChannel;
 import org.tmatesoft.hg.repo.HgDataFile;
-import org.tmatesoft.hg.repo.HgRepository;
 
 /**
  *
@@ -35,13 +33,24 @@ public class TestByteChannel {
 	public static void main(String[] args) throws Exception {
 		RepositoryFacade rf = new RepositoryFacade();
 		rf.init();
-		HgDataFile file = rf.getRepository().getFileNode("COPYING");
-		int rev = HgRepository.TIP;
+		HgDataFile file = rf.getRepository().getFileNode("TODO");
+		for (int i = file.getRevisionCount() - 1; i >= 0; i--) {
+			System.out.print("Content for revision:" + i);
+			compareContent(file, i);
+			System.out.println(" OK");
+		}
+		//CatCommand cmd = rf.createCatCommand();
+	}
+
+	private static void compareContent(HgDataFile file, int rev) throws Exception {
 		byte[] oldAccess = file.content(rev);
 		ByteArrayChannel ch = new ByteArrayChannel();
 		file.content(rev, ch);
 		byte[] newAccess = ch.toArray();
 		Assert.assertArrayEquals(oldAccess, newAccess);
-		//CatCommand cmd = rf.createCatCommand();
+		// don't trust anyone (even JUnit) 
+		if (!Arrays.equals(oldAccess, newAccess)) {
+			throw new RuntimeException("Failed:" + rev);
+		}
 	}
 }
